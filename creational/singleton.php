a6f43a90b9881ff8b3923fb873b5225d3cbd7a88
sodaler <misha_lector87@mail.ru>
2023-06-13 14:46:08 +0400

    Creational. Singleton

---
+++ creational/singleton.php
@@ -0,0 +1,48 @@
+<?php
+
+final class Connection
+{
+    private static ?self $instance = null;
+    private static string $name;
+
+    public static function getInstance(): self
+    {
+        if (self::$instance == null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function __clone(): void
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    public function __wakeup(): void
+    {
+        // TODO: Implement __wakeup() method.
+    }
+
+    /**
+     * @return string
+     */
+    public static function getName(): string
+    {
+        return self::$name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public static function setName(string $name): void
+    {
+        self::$name = $name;
+    }
+}
+
+$connection = Connection::getInstance();
+$connection::setName('Laravel');
+
+$connection2 = Connection::getInstance();
+var_dump($connection2::getName());
