4cd2726f69301562d3ce835b82c6ec78f9a1bdc8
sodaler <misha_lector87@mail.ru>
2023-06-14 16:09:04 +0400

    Structural. Registry

---
+++ structural/registry.php
@@ -0,0 +1,32 @@
+<?php
+
+abstract class Registry
+{
+    private static array $services = [];
+
+    final public static function setService($key, Service $service)
+    {
+        self::$services[$key] = $service;
+    }
+
+    final public static function getService($key): string|Service
+    {
+        if (isset(self::$services[$key])) {
+            return self::$services[$key];
+        }
+        return 'this service doesnt exists';
+    }
+}
+
+class Service
+{
+
+}
+
+$service = new Service();
+
+Registry::setService(1, $service);
+
+$service = Registry::getService(1);
+
+var_dump($service);
