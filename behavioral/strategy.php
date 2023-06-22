8ae3cc01ed226dcc861fed60ee46fe2a959e7dd1
sodaler <misha_lector87@mail.ru>
2023-06-22 17:05:46 +0400

    Behavioral. Strategy

---
+++ behavioral/strategy.php
@@ -0,0 +1,63 @@
+<?php
+
+interface Definer
+{
+    public function define($arg): string;
+}
+
+class Data
+{
+    private Definer $definer;
+    private int|string|bool $arg;
+
+    /**
+     * @param bool|int|string $arg
+     */
+    public function setArg(bool|int|string $arg): void
+    {
+        $this->arg = $arg;
+    }
+
+    /**
+     * @param Definer $definer
+     */
+    public function __construct(Definer $definer)
+    {
+        $this->definer = $definer;
+    }
+
+    public function executeStrategy(): string
+    {
+        return $this->definer->define($this->arg);
+    }
+}
+
+class IntDefiner implements Definer
+{
+    public function define($arg): string
+    {
+        return $arg . ' from int strategy';
+    }
+}
+
+class StringDefiner implements Definer
+{
+    public function define($arg): string
+    {
+        return $arg . ' from string strategy';
+    }
+}
+
+class BoolDefiner implements Definer
+{
+    public function define($arg): string
+    {
+        return $arg . ' from bool strategy';
+    }
+}
+
+$data = new Data(new StringDefiner());
+
+$data->setArg('some arg for first');
+
+var_dump($data->executeStrategy());
